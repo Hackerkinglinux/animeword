485857ab2a4723168a54f992c4b7535660bf3f27
Hackerkinglinux <143250811+Hackerkinglinux@users.noreply.github.com>
2024-07-08 15:27:43 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+    // Inicialmente mostramos el contenido de la primera pestaña
+    $('#series').addClass('active');
+
+    // Cambiar de pestaña al hacer clic en un enlace de las tabs
+    $('.tabs a').on('click', function(e) {
+        e.preventDefault();
+        var target = $(this).attr('href');
+        $('.content-section').removeClass('active');
+        $(target).addClass('active');
+    });
+
+    // Configuración del carrusel básico
+    var currentIndex = 0;
+    var items = $('.carousel-item');
+    var itemAmount = items.length;
+
+    function cycleItems() {
+        var item = $('.carousel-item').eq(currentIndex);
+        items.hide();
+        item.css('display', 'block');
+    }
+
+    var autoSlide = setInterval(function() {
+        currentIndex += 1;
+        if (currentIndex > itemAmount - 1) {
+            currentIndex = 0;
+        }
+        cycleItems();
+    }, 3000); // Cambia cada 3 segundos, ajusta el valor según lo que necesites
+
+    // Any JavaScript or jQuery to initialize your carousel
+    cycleItems(); // Inicializa la primera imagen del carrusel
+});
+
