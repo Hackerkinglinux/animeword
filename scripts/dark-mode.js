b266f5c466a5350344ef3523a5779169ce21bed7
Hackerkinglinux <143250811+Hackerkinglinux@users.noreply.github.com>
2024-07-18 12:19:36 -0400

    Add files via upload

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,12 +1,12 @@
 // scripts/dark-mode.js
 document.addEventListener('DOMContentLoaded', function() {
     const toggleDarkModeButton = document.getElementById('toggleDarkMode');
+
     toggleDarkModeButton.addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
-        document.querySelector('header').classList.toggle('dark-mode');
-        document.querySelector('footer').classList.toggle('dark-mode');
-        document.querySelector('.advanced-search').classList.toggle('dark-mode');
-        document.querySelector('.anime-row').classList.toggle('dark-mode');
-        document.querySelector('.carousel-container').classList.toggle('dark-mode');
+        
+        // Aplicar o quitar la clase 'dark-mode' a los elementos relevantes
+        document.querySelectorAll('header, footer, .advanced-search, .anime-row, .carousel-container')
+                .forEach(el => el.classList.toggle('dark-mode'));
     });
 });
