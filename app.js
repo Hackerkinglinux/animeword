c82b9e0bda01540ee373d53f83a3dc63c4485f83
Hackerkinglinux <143250811+Hackerkinglinux@users.noreply.github.com>
2024-07-20 03:50:31 -0400

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-// You can add JavaScript functionality as needed
-// Example: Fetching data, handling interactions, etc.
-// app.js
-
-function toggleGenres() {
-    var dropdown = document.getElementById("genre-dropdown");
-    dropdown.classList.toggle("show");
-}
