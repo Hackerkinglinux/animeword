c1ffe30735b6ef53f1818b5f43804f538241b870
Hackerkinglinux <143250811+Hackerkinglinux@users.noreply.github.com>
2024-07-22 10:37:16 -0400

    Add files via upload

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,4 +1,21 @@
 // scripts/dark-mode.js
-document.getElementById('toggleDarkMode').addEventListener('click', function() {
-    document.body.classList.toggle('dark-mode');
+document.addEventListener('DOMContentLoaded', function() {
+    const darkModeToggle = document.getElementById('toggleDarkMode');
+    const darkModeClass = 'dark-mode';
+
+    // Verifica si el modo oscuro está guardado en localStorage
+    if (localStorage.getItem('dark-mode') === 'enabled') {
+        document.body.classList.add(darkModeClass);
+    }
+
+    darkModeToggle.addEventListener('click', function() {
+        document.body.classList.toggle(darkModeClass);
+        
+        // Guarda el estado en localStorage
+        if (document.body.classList.contains(darkModeClass)) {
+            localStorage.setItem('dark-mode', 'enabled');
+        } else {
+            localStorage.setItem('dark-mode', 'disabled');
+        }
+    });
 });
