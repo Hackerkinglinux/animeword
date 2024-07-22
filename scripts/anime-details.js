7adc3bb8fcdb035afa0b8a10c4361ee52dd6cdea
Hackerkinglinux <143250811+Hackerkinglinux@users.noreply.github.com>
2024-07-22 10:46:40 -0400

    Add files via upload

---
+++ scripts/anime-details.js
@@ -0,0 +1,6 @@
+// scripts/anime-details.js
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('watchNow').addEventListener('click', function() {
+        window.location.href = 'https://tu-enlace-a-streaming.com';
+    });
+});
