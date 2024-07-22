1c304b20cf3915989780f99507f117a1e1e2b175
Hackerkinglinux <143250811+Hackerkinglinux@users.noreply.github.com>
2024-07-22 11:39:18 -0400

    Add files via upload

--- scripts/dark-mode.js
+++ scripts/dark-mode.js
@@ -1,12 +1,16 @@
 $(document).ready(function () {
     const darkModeToggle = $('#darkModeToggle');
+    const nightLightToggle = $('#nightLightToggle');
 
-    // Comprobar estado del modo oscuro al cargar la página
+    // Comprobar el estado del modo oscuro y luz nocturna al cargar la página
     if (localStorage.getItem('darkMode') === 'enabled') {
         $('body').addClass('dark-mode');
+        if (localStorage.getItem('nightLight') === 'enabled') {
+            $('body').addClass('night-light');
+        }
     }
 
-    // Alternar modo oscuro al hacer clic en el botón
+    // Alternar el modo oscuro al hacer clic en el botón
     darkModeToggle.on('click', function () {
         $('body').toggleClass('dark-mode');
 
@@ -15,6 +19,18 @@ $(document).ready(function () {
             localStorage.setItem('darkMode', 'enabled');
         } else {
             localStorage.setItem('darkMode', 'disabled');
+        }
+    });
+
+    // Alternar el efecto de luz nocturna al hacer clic en el botón
+    nightLightToggle.on('click', function () {
+        $('body').toggleClass('night-light');
+
+        // Guardar la preferencia en el almacenamiento local
+        if ($('body').hasClass('night-light')) {
+            localStorage.setItem('nightLight', 'enabled');
+        } else {
+            localStorage.setItem('nightLight', 'disabled');
         }
     });
 });
